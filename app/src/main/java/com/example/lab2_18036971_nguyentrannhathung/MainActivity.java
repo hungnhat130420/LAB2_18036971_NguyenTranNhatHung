cb8be3f30660ae452a4318e8ab5b445915237de5
Nhat Hung <hungnguyen.130420@gmail.com>
2021-03-24 10:24:21 +0700

    lab2 android

---
+++ app/src/main/java/com/example/lab2_18036971_nguyentrannhathung/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab2_18036971_nguyentrannhathung;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
